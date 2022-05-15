4db910f183e9fe52cc7ca557480b281b610daaf1
LordRonz <aarontanhar2000@gmail.com>
2022-05-15 14:58:26 +0700

    fix: disable preact :rocket:

--- next.config.js
+++ next.config.js
@@ -1,16 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  webpack: (config, { dev, isServer }) => {
-    // Replace React with Preact only in client production build
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat',
-      });
-    }
-
+  webpack: (config) => {
     return config;
   },
 };
